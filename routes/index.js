2e47bb2f6c7af79fd2463163d8178c6fb987033a
Pramod Jain <jpramod@TOSHIBA-TSIP.COM>
2018-12-13 11:33:31 +0530

    add logs in req handler

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    console.log("Request received for home page");
   res.render('index', { title: 'Pramod\'s' });
 });
 
